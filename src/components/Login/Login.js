b07c9e8a86c54ec74b8bf8d6c727a120a6d768e1
Irina Soboleva <iralsob@gmail.com>
2023-01-16 15:54:58 +0100

    try clean up for useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=> {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const timerId = setTimeout(()=>{
+      console.log('VALIDATION');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    },500);
+    
+    return ()=>{
+      console.log('CLEAN UP');
+      clearTimeout(timerId);
+    }
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
